9f4be4749fb66289b5588e8f9484344b153d802b
koly <koly@DESKTOP-HT297RI>
2021-06-21 18:30:41 +0300

    Добавьте файлы проекта.

---
+++ example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace example
+{
+
+	class Program
+	{
+		static void Main()
+		{
+			String[] names = { "Eugene", "Katherina", "John", "Stephen", "Phil", "Joe", "Alex" };
+			int[] times = { 172, 169, 191, 202, 181, 191, 201 };
+			int max = times[0];
+			int index = 0;
+			for (int i = 0; i < times.Length; ++i) 
+			{
+				if (max < times[i])
+				{
+					max = times[i];
+					index = i;
+
+				}
+			}
+
+			Console.WriteLine($"Name = {0} Height: {1}$", names[index], max);
+		}
+	}
+
+}
